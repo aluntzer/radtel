f5f2b7692822363b6f33791c9565bf4ead48a014
Armin Luntzer <armin.luntzer@univie.ac.at>
2017-11-15 16:24:20 +0100

    add more missing files

---
+++ src/client/sig/sig_cmd_success.c
@@ -0,0 +1,30 @@
+/**
+ * @file    client/sig/sig_cmd_success.c
+ * @author  Armin Luntzer (armin.luntzer@univie.ac.at)
+ *
+ * @copyright GPLv2
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ */
+
+#include <glib.h>
+#include <glib-object.h>
+#include <signals.h>
+
+/**
+ * @brief emit cmd-success signal
+ */
+
+void sig_cmd_success(void)
+{
+	g_message("Emit signal \"cmd-success\"");
+
+	g_signal_emit_by_name(sig_get_instance(), "cmd-success");
+}
