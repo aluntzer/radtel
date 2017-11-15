1edb40d4145c156fc8761f388c137cb97b466d88
Armin Luntzer <armin.luntzer@univie.ac.at>
2017-11-15 16:21:21 +0100

    add missing file

---
+++ src/client/include/signals.h
@@ -0,0 +1,31 @@
+/**
+ * @file    client/include/signals.h
+ * @author  Armin Luntzer (armin.luntzer@univie.ac.at)
+ *
+ * @copyright GPLv2
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ */
+
+#ifndef _CLIENT_INCLUDE_SIGNALS_H_
+#define _CLIENT_INCLUDE_SIGNALS_H_
+
+#include <protocol.h>
+
+void sig_cmd_success(void);
+void sig_cmd_capabilities(const struct capabilities *c);
+
+
+
+gpointer *sig_get_instance(void);
+void sig_init(void);
+
+#endif /* _CLIENT_INCLUDE_SIGNALS_H_ */
+
